6544007febebc29896fd2e3fef2a2bae5dda807a
Dama <guitarepower@gmail.com>
2018-11-08 16:39:45 -0500

    Added pause menu

---
+++ UltimateTablePong/Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenuController : MonoBehaviour {
+
+    private bool playerReadyState;
+    private bool paused;
+    private GameObject pauseTitle;
+    private GameObject pausePanel; 
+    private GameObject playerReady;
+    private GameObject pauseMainMenuButton;
+    private GameObject pauseResumeButton;
+
+    // Use this for initialization
+    void Start () {
+        pauseTitle = GameObject.Find("PauseTitle");
+        pausePanel = GameObject.Find("PausePanel");
+        playerReady = GameObject.Find("PlayerReady");
+        pauseMainMenuButton = GameObject.Find("PauseMainMenuButton");
+        pauseResumeButton = GameObject.Find("PauseResumeButton");
+
+        paused = false;
+        pauseTitle.SetActive(false);
+        pausePanel.SetActive(false);
+        pauseMainMenuButton.SetActive(false);
+        pauseResumeButton.SetActive(false);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused == false)
+            {
+                DisplayPauseMenu();
+            }
+            else
+            {
+                HidePauseMenu();
+            }
+        }
+	}
+
+    void DisplayPauseMenu()
+    {
+        Time.timeScale = 0;
+        paused = true;
+        pauseTitle.SetActive(true);
+        pausePanel.SetActive(true);
+        pauseMainMenuButton.SetActive(true);
+        pauseResumeButton.SetActive(true);
+
+        playerReadyState = playerReady.activeSelf;
+        playerReady.SetActive(false);
+
+        pauseResumeButton.GetComponent<Button>().onClick.AddListener(HidePauseMenu);
+    }
+
+    void HidePauseMenu()
+    {
+        Time.timeScale = 1;
+        paused = false;
+        pauseTitle.SetActive(false);
+        pausePanel.SetActive(false);
+        pauseMainMenuButton.SetActive(false);
+        pauseResumeButton.SetActive(false);
+
+        playerReady.SetActive(playerReadyState);
+    }
+}
