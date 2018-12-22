97ccbf07f10f7d57d8d116aa6ca9b97f263808fc
Dama <guitarepower@gmail.com>
2018-12-22 17:42:14 -0500

    change pitch changing with new system

--- UltimateTablePong/Assets/Scripts/SoundManager.cs
+++ UltimateTablePong/Assets/Scripts/SoundManager.cs
@@ -8,12 +8,12 @@ public class SoundManager : MonoBehaviour
 
     private AudioClip sfxHitPin1Clip;
 
-    private AudioSource introMusic;
-    private AudioSource inGame1;
-    private AudioSource inGame2;
-    private AudioSource endGame;
+    private GameObject introMusic;
+    private GameObject inGame1;
+    private GameObject inGame2;
+    private GameObject endGame;
 
-    private AudioSource currentSong;
+    private GameObject currentSong = null;
 
     private AudioSource sfxSource;
     private AudioSource musicSource;
@@ -34,10 +34,10 @@ public class SoundManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        introMusic = GameObject.Find("IntroMusic").GetComponent<AudioSource>();
-        inGame1 = GameObject.Find("InGame1Music").GetComponent<AudioSource>();
-        inGame2 = GameObject.Find("InGame2Music").GetComponent<AudioSource>();
-        endGame = GameObject.Find("EndClipMusic").GetComponent<AudioSource>();
+        introMusic = GameObject.Find("IntroMusic");
+        inGame1 = GameObject.Find("InGame1Music");
+        inGame2 = GameObject.Find("InGame2Music");
+        endGame = GameObject.Find("EndClipMusic");
     }
 
     public void changeMusicPitch(float pitch)
@@ -67,55 +67,58 @@ public class SoundManager : MonoBehaviour
 
     public void playMusic(string name)
     {
-        introMusic.Stop();
-        inGame1.Stop();
-        inGame2.Stop();
-        endGame.Stop();
+        introMusic.GetComponent<AudioSource>().Stop();
+        inGame1.GetComponent<AudioSource>().Stop();
+        inGame2.GetComponent<AudioSource>().Stop();
+        endGame.GetComponent<AudioSource>().Stop();
         switch (name)
         {
             case "mainMenu":
-                introMusic.Play();
+                introMusic.GetComponent<AudioSource>().Play();
                 currentSong = introMusic;
                 break;
             case "inGame":
                 switch (chosenSong)
                 {
                     case "Barge":
-                        inGame1.Play();
+                        inGame1.GetComponent<AudioSource>().Play();
                         currentSong = inGame1;
                         break;
                     case "Moskito":
-                        inGame2.Play();
+                        inGame2.GetComponent<AudioSource>().Play();
                         currentSong = inGame2;
                         break;
                 }
                 break;
             case "gameEnd":
-                endGame.Play();
+                endGame.GetComponent<AudioSource>().Play();
                 currentSong = endGame;
                 break;
+        }
+    }
+
+    public void changePitchCurrentSong()
+    {
+        if (currentSong.GetComponent<AudioSource>().pitch < lastDesiredPitch)
+        {
+            Debug.Log("Nice");
+            currentSong.GetComponent<AudioSource>().pitch = currentSong.GetComponent<AudioSource>().pitch + 0.1F;
+        }
+        else if (currentSong.GetComponent<AudioSource>().pitch > lastDesiredPitch)
+        {
+            Debug.Log("Nicr");
+            currentSong.GetComponent<AudioSource>().pitch  = currentSong.GetComponent<AudioSource>().pitch - 0.1F;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (musicPitch < lastDesiredPitch)
-        {
-            musicPitch += 0.01F;
-        }
-        else if (musicPitch > lastDesiredPitch)
-        {
-            musicPitch -= 0.01F;
-        }
-        introMusic.pitch = musicPitch;
-        inGame1.pitch = musicPitch;
-        inGame2.pitch = musicPitch;
-        endGame.pitch = musicPitch;
+        changePitchCurrentSong();
 
-        introMusic.volume = musicVolume;
-        inGame1.volume = musicVolume;
-        inGame2.volume = musicVolume;
-        endGame.volume = musicVolume;
+        introMusic.GetComponent<AudioSource>().volume = musicVolume;
+        inGame1.GetComponent<AudioSource>().volume = musicVolume;
+        inGame2.GetComponent<AudioSource>().volume = musicVolume;
+        endGame.GetComponent<AudioSource>().volume = musicVolume;
     }
 }
